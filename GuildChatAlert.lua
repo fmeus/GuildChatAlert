0ecc5740174da24a1c2d553d528336c131c39a50
fmeus <frank.meus@gmail.com>
2014-11-03 21:51:13 +0100

    Update for 6.0.x

--- GuildChatAlert.lua
+++ GuildChatAlert.lua
@@ -1,8 +1,24 @@
 -- Create frame for responding to game events
 local f = CreateFrame( "Frame", "GuildChatAlert", UIParent );
 
+-- Gratz variants
+local messages = { "gratz","Gratz","gz","Gz" };
+
 -- Current player (toon-realm)
 local currentPlayer = UnitName( "player" ).."-"..GetRealmName();
+local lastTrigger, delayTime, totalTime, sendMessage = 0, 10, 0, false;
+
+-- Sende gratz to guild chat after n seconds (determined by delayTime)
+local function OnUpdate( self, elapsed )
+	totalTime = totalTime + elapsed;
+
+	if ( totalTime >= delayTime and sendMessage ) then
+		sendMessage, totalTime = false, 0;
+		local toss = random( 1, getn( messages ) );
+
+		SendChatMessage( messages[toss], "GUILD" );
+	end;
+end;
 
 -- Handle event CHAT_MSG_GUILD
 function f:CHAT_MSG_GUILD( event, ... )
@@ -22,6 +38,22 @@ function f:CHAT_MSG_OFFICER( event, ... )
 	end;
 end;
 
+-- Handle event CHAT_MSG_GUILD_ACHIEVEMENT
+function f:CHAT_MSG_GUILD_ACHIEVEMENT( event, ... )
+	local message, sender = ...;
+	local interval = time() - lastTrigger;
+	local playerAFK = UnitIsAFK( "player" ) or 0;
+
+	if ( sender ~= currentPlayer ) then
+		if ( interval >= 60 and playerAFK == 0 ) then
+			totalTime = 0;
+			sendMessage = true;
+			lastTrigger = time();
+		end;
+		PlaySoundFile("Sound\\Spells\\AchievmentSound1.wav");
+	end;
+end;
+
 -- Generic event handler
 local function OnEvent( self, event, ... )
 	self[event]( self, event, ... );
@@ -29,7 +61,9 @@ end;
 
 -- Setup event handler
 f:SetScript( "OnEvent", OnEvent );
+f:SetScript( "OnUpdate", OnUpdate );
 
 -- Register events to listen to
 f:RegisterEvent( "CHAT_MSG_GUILD" );
 f:RegisterEvent( "CHAT_MSG_OFFICER" );
+f:RegisterEvent( "CHAT_MSG_GUILD_ACHIEVEMENT" );
